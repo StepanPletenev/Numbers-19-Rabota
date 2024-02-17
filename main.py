9f3731b94c593cd5cefe4e2393c10e9d3314440c
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2024-02-17 08:55:55 +0300

    Цифры всё!!!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import random
 
-pygame.init()
+if __name__ == "__main__":
+    pygame.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -36,7 +37,7 @@ numbers = []
 for i in range(1,9):
     numbers.append(str(i))
 
-lines = []
+lines = lines = []
 for i in range(10):
     line = ""
     for j in range(10):
@@ -59,40 +60,26 @@ def draw_board(selected_numbers=None):
                 draw_text(lines[i][j], font, WHITE, j * 50 + 25, i * 50 + 25)
 
 def can_remove(x1, y1, x2, y2):
-    if x1 == x2:
-        for i in range(min(y1, y2), max(y1, y2) + 1):
-            if lines[x1][i] == " ":
-                return False
-            elif not get_sum(lines[x1][i], lines[x1][y1]):
-                return False
-        return True
-    elif y1 == y2:
-        for i in range(min(x1, x2), max(x1, x2) + 1):
-            if lines[i][y1] == " ":
-                return False
-            elif not get_sum(lines[i][y1], lines[x1][y1]):
-                return False
+    if x1 < 0 or x1 >= len(lines) or y1 < 0 or y1 >= len(lines[0]) or x2 < 0 or x2 >= len(lines) or y2 < 0 or y2 >= len(lines[0]):
+        return False
+
+    if lines[x1][y1] == " " or lines[x2][y2] == " ":
+        return False
+
+    num1 = int(lines[x1][y1])
+    num2 = int(lines[x2][y2])
+
+    if num1 + num2 == 10:
         return True
     else:
-        sum_positions = get_sum_positions()
-        for pos1, pos2 in sum_positions:
-            if (x1, y1) in pos1 and (x2, y2) in pos2:
-                return True
         return False
 
 def remove_numbers(x1, y1, x2, y2):
-    if x1 == x2:
-        for i in range(min(y1, y2), max(y1, y2) + 1):
-            lines[x1] = lines[x1][:i] + " " + lines[x1][i+1:]
-    elif y1 == y2:
-        for i in range(min(x1, x2), max(x1, x2) + 1):
-            lines[i] = lines[i][:y1] + " " +    lines[i][y1+1:]
-    else:
-        sum_positions = get_sum_positions()
-        for pos1, pos2 in sum_positions:
-            if (x1, y1) in pos1 and (x2, y2) in pos2:
-                for x, y in pos1 + pos2:
-                    lines[x] = lines[x][:y] + " " + lines[x][y+1:]
+    if x1 < 0 or x1 >= len(lines) or y1 < 0 or y1 >= len(lines[0]) or x2 < 0 or x2 >= len(lines) or y2 < 0 or y2 >= len(lines[0]):
+        return
+
+    lines[x1] = lines[x1][:y1] + " " + lines[x1][y1+1:]
+    lines[x2] = lines[x2][:y2] + " " + lines[x2][y2+1:]
 
 def get_sum(num1, num2):
     if num1 == '' or num2 =='':
@@ -101,31 +88,32 @@ def get_sum(num1, num2):
         return True
     else:
         return False
-
 def game():
     running = True
     selected_numbers = []
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 x = pos[1] // 50
                 y = pos[0] // 50
-                if len(selected_numbers) == 0:
+
+                if len(selected_numbers) < 2:
                     if lines[x][y] != " ":
                         selected_numbers.append((x, y))
-                elif len(selected_numbers) == 1:
-                    if (x, y) != selected_numbers[0]:
-                        if lines[x][y] != " " and (
-                                can_remove(selected_numbers[0][0], selected_numbers[0][1], x, y) or get_sum(
-                                lines[selected_numbers[0][0]][selected_numbers[0][1]], lines[x][y])):
-                            remove_numbers(selected_numbers[0][0], selected_numbers[0][1], x, y)
-                        selected_numbers.clear()
-                else:
-                    selected_numbers.clear()
+
+                if len(selected_numbers) == 2:
+                    x1, y1 = selected_numbers[0]
+                    x2, y2 = selected_numbers[1]
+
+                    if can_remove(x1, y1, x2, y2):
+                        remove_numbers(x1, y1, x2, y2)
+                        selected_numbers = []
+                    else:
+                        selected_numbers = []
 
         game_screen.fill(WHITE)
         draw_board(selected_numbers)
@@ -135,25 +123,13 @@ def game():
             if " " not in line:
                 game_over = False
                 break
+
         if game_over:
             draw_text("Вы выиграли!", font, BLACK, WIDTH // 2, HEIGHT // 2)
 
         pygame.display.flip()
 
-def main_menu():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                if 200 <= pos[0] <= 400 and 300 <= pos[1] <= 350:
-                    return
+    pygame.quit()
 
-        draw_menu()
-        pygame.display.flip()
-
-main_menu()
 game()
 pygame.quit()
