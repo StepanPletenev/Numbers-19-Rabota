2540817a26cb2f894e92a0426b5dddf652b12f9a
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2024-05-03 12:19:03 +0300

    Таймер

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import sys
 import random
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QDesktopWidget, QLayout
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QGridLayout, QPushButton, QDesktopWidget, QLayout
+
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Main Menu")
+        self.setGeometry(100, 100, 400, 200)
+
+        start_button = QPushButton("Start Game", self)
+        start_button.setGeometry(150, 75, 100, 50)
+        start_button.clicked.connect(self.start_game)
+    def start_game(self):
+        self.hide()
+        app = QApplication.instance()
+        game = NumberGame()
+        game.show()
+        app.exec_()
 
 class NumberGame(QWidget):
     def __init__(self):
@@ -14,6 +31,7 @@ class NumberGame(QWidget):
 
     def closeEvent(self, event):
         sys.exit()
+
     def initUI(self):
         self.setWindowTitle("Number19")
         self.setGeometry(100, 100, 800, 800)
@@ -23,6 +41,9 @@ class NumberGame(QWidget):
         self.layout.setSizeConstraint(QLayout.SetFixedSize)
         self.lines = []
         self.selected_numbers = []
+        self.timer = QTimer()
+        self.time = 0
+        self.timer_label = None
 
         for i in range(10):
             line = ""
@@ -40,13 +61,26 @@ class NumberGame(QWidget):
                 btn.clicked.connect(lambda _, x=i, y=j: self.number_clicked(x, y))
                 self.layout.addWidget(btn, i, j)
 
+        timer_label_layout = QGridLayout()
+        self.timer_label = QPushButton("00:00", self)
+        self.timer_label.setFixedSize(80, 30)
+        self.timer_label.setStyleSheet("background-color: white; font-size: 20px;")
+        timer_label_layout.addWidget(self.timer_label, 0, 0, 1, 1)
+        self.layout.addLayout(timer_label_layout, 11, 9, 1, 1)
+
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_timer)
+        self.timer.start()
+
         self.setLayout(self.layout)
         self.move_center()
+
     def move_center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
     def number_clicked(self, x, y):
         if len(self.selected_numbers) == 2:
             self.remove_selected_numbers()
@@ -58,6 +92,7 @@ class NumberGame(QWidget):
             self.selected_numbers.append((x, y))
 
         self.update_buttons()
+
     def remove_selected_numbers(self):
         if len(self.selected_numbers) == 2:
             x1, y1 = self.selected_numbers[0]
@@ -66,6 +101,7 @@ class NumberGame(QWidget):
                 self.remove_numbers(x1, y1, x2, y2)
             self.selected_numbers = []
             self.update_buttons()
+
     def can_remove(self, x1, y1, x2, y2):
         if (0 <= x1 < len(self.lines) and 0 <= y1 < len(self.lines[0])
                 and 0 <= x2 < len(self.lines) and 0 <= y2 < len(self.lines[0])):
@@ -76,6 +112,7 @@ class NumberGame(QWidget):
                         (abs(x1 - x2) == 1 and y1 == y2) or (abs(y1 - y2) == 1 and x1 == x2)):
                     return True
         return False
+
     def remove_numbers(self, x1, y1, x2, y2):
         if x1 > x2 or (x1 == x2 and y1 > y2):
             x1, y1, x2, y2 = x2, y2, x1, y1
@@ -91,6 +128,7 @@ class NumberGame(QWidget):
         self.lines[0][y2] = " "
 
         self.update_buttons()
+
     def update_buttons(self):
         for i in range(10):
             for j in range(10):
@@ -105,6 +143,12 @@ class NumberGame(QWidget):
                         "background-color: white")
                 if (i, j) in self.selected_numbers:
                     button.setStyleSheet("background-color: green")
+
+    def update_timer(self):
+        self.time += 1
+        minutes = self.time // 60
+        seconds = self.time % 60
+        self.timer_label.setText(f"{minutes:02}:{seconds:02}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
